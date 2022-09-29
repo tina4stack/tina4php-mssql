c0a3c04cf782131ac0a1627b287770ad693cca30
Andre van Zuydam <andrevanzuydam@gmail.com>
2022-09-29 16:07:03 +0200

    Fix for order by in select where count of records is required

--- Tina4/MSSQLQuery.php
+++ Tina4/MSSQLQuery.php
@@ -63,6 +63,9 @@ class MSSQLQuery extends DataConnection implements DataBaseQuery
                     if (stripos($sql, "@") !== false || stripos($sql, "sp_") !== false) {
                         $resultCount["COUNT_RECORDS"] = count($records);
                     } else {
+
+                        $initialSQL = explode("order", strtolower($initialSQL))[0];
+
                         $sqlCount = "select count(*) as COUNT_RECORDS from ($initialSQL) as tcount";
 
                         $recordCount = \sqlsrv_query($this->getDbh(), $sqlCount);
